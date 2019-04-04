bfc7a70e05d24f82204ef0992a1b82cdaf3c0b66
Martin Sehnoutka <msehnout@redhat.com>
2019-04-04 15:39:22 +0200

    improve example

--- examples/rmstub.py
+++ examples/rmstub.py
@@ -2,6 +2,7 @@ import mb
 import time
 import logging
 import os
+import random
 
 logger = logging.getLogger(__file__)
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
@@ -11,16 +12,17 @@ logging.basicConfig(level=LOGLEVEL)
 logging.info("starting")
 time.sleep(21)
 
-for topic in [mb.path.TOPIC_RELEASE_MONITORING_NPM, mb.path.TOPIC_RELEASE_MONITORING_PYPI]:
-    producer = mb.MbProducer(topic)
-    for _ in range(10):
-        try:
-            msg = '{ "foo": "bar" }'
-            producer.publish(msg)
-            print("Publishing ...")
-            time.sleep(1)
-        except KeyboardInterrupt:
-            producer.disconnect()
-            exit(0)
+producer1 = mb.MbProducer(mb.path.TOPIC_RELEASE_MONITORING_NPM)
+producer2 = mb.MbProducer(mb.path.TOPIC_RELEASE_MONITORING_PYPI)
+while True:
+    try:
+        producer = random.choice([producer1, producer2])
+        msg = '{ "foo": "bar" }'
+        producer.publish(msg)
+        print("Publishing ...")
+        time.sleep(1)
+    except KeyboardInterrupt:
+        producer.disconnect()
+        exit(0)
 
-    producer.disconnect()
+producer.disconnect()

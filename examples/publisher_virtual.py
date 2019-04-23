b41457af9f325546883f8956b293a5ff30ce5ac0
Martin Sehnoutka <msehnout@redhat.com>
2019-04-23 16:11:46 +0200

    improve producer

--- examples/publisher_virtual.py
+++ examples/publisher_virtual.py
@@ -5,6 +5,8 @@ import os
 
 logger = logging.getLogger(__file__)
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+PAUSE = int(os.environ.get('PAUSE', '15'))
+
 logging.basicConfig(level=LOGLEVEL)
 
 
@@ -18,7 +20,7 @@ while True:
         msg = '{ "foo": "' + str(counter) + '" }'
         producer.publish(msg)
         logger.info("Publishing ..." + str(counter))
-        time.sleep(5)
+        time.sleep(PAUSE)
         counter += 1
     except KeyboardInterrupt:
         producer.disconnect()
